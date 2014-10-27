c454371ce123f1112fa060aa1a22f7eadf0ed36c
Yatish <yatishby@gmail.com>
2014-10-27 22:24:37 +0800

    Socket handler to clear all sockets in a room

--- src/flaskapp/main/sockets.py
+++ src/flaskapp/main/sockets.py
@@ -4,11 +4,14 @@ from flask.ext.socketio import emit, join_room, leave_room
 from .. import socketio
 
 socketRooms = {}
+defaultRoom = str(0)
+roomIndex = 1
 
 
 @socketio.on('connect', namespace='/test')
 def socketConnect():
     session['id'] = str(uuid.uuid4())
+    session['room'] = defaultRoom
     emit('my response', {'data': 'Connected ' + session['id']})
 
 @socketio.on('disconnect', namespace='/test')
@@ -18,7 +21,60 @@ def socketDisconnect():
 @socketio.on('print connected', namespace='/test')
 def printSocketsConnected():
 	for sessid, socket in request.namespace.socket.server.sockets.items():
-		emit('my response', {'data': 'sessions uuid: ' + socket['/test'].session['id']}) 
+		emit('my response', {'data': 'sessions uuid: ' + socket['/test'].session['id']})
+
+@socketio.on('joinroom', namespace='/test')
+def joinRoom(room):
+	if session['room'] == defaultRoom:
+		# room = str(roomIndex)
+		room = room['room']
+		session['room'] = room
+		join_room(room)
+		if socketRooms.has_key(room) == True :
+			usersInRoom = socketRooms.get(room)
+			usersInRoom.append(session['id'])
+			socketRooms[room] = usersInRoom
+		else:
+			usersInRoom = []
+			usersInRoom.append(session['id'])
+			socketRooms[room] = usersInRoom
+		# roomIndex +=1
+		emit('my response', {'data': 'Joined room'})
+	else:
+		emit('my response', {'data': 'Already part of a room'})
+
+@socketio.on('leaveroom', namespace='/test')
+def leaveRoom():
+	if session['room'] != defaultRoom:
+		room = session['room']
+		leave_room(room)
+		if socketRooms.has_key(room) == True:
+			usersInRoom = socketRooms.get(room)
+			usersInRoom.remove(session['id'])
+			socketRooms[room] = usersInRoom
+		session['room'] = defaultRoom
+		emit('my response', {'data': 'left room '+room})
+	else:
+		emit('my response', {'data': 'wasn\'t part of a room'})
+
+@socketio.on('clearroom', namespace='/test')
+def clearRoom(room):
+	room = room['room']
+	if socketRooms.has_key(room) == True:
+		usersInRoom = socketRooms.get(room)
+		del socketRooms[room]
+		for eachUser in usersInRoom :
+			for sessid, socket in request.namespace.socket.server.sockets.items():
+				if socket['/test'].session['id'] == eachUser:
+					socket['/test'].session['room'] = defaultRoom
+					socket['/test'].leave_room(room)
+					socket['/test'].base_emit('my response', {'data': "cleared room"})
+		# emit('my response', {'data': "checking for room"}, room=room) # This doesn't send message to any client. Verified
+	else:
+		emit('my response', {'data': 'no such room'})
+
+
+
 
 # @socketio.on('client name', namespace='/test')
 # def test_message(message):
