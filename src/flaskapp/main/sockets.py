fa95b998d4b3d16dadecceb3c567a742b0f5ab69
Yatish <yatishby@gmail.com>
2014-10-28 01:29:08 +0800

    Socket handlers that read answers and send ans to room when answers received from both clients

--- src/flaskapp/main/sockets.py
+++ src/flaskapp/main/sockets.py
@@ -1,15 +1,17 @@
-import uuid
+import uuid, json
 from flask import Flask, render_template, session, request
 from flask.ext.socketio import emit, join_room, leave_room
 from .. import socketio
 
 socketRooms = {}
+roomReceivedAnswers = {}
 defaultRoom = str(0)
 roomIndex = 1
 
 
 @socketio.on('connect', namespace='/test')
 def socketConnect():
+	# Could substitute id with some cookie user variable
     session['id'] = str(uuid.uuid4())
     session['room'] = defaultRoom
     emit('my response', {'data': 'Connected ' + session['id']})
@@ -73,70 +75,28 @@ def clearRoom(room):
 	else:
 		emit('my response', {'data': 'no such room'})
 
+@socketio.on('readanswer', namespace='/test')
+def readAnswerByClient(message):
+	room = session['room']
+	idClient = session['id']
+	idQuestion = message['qid']
+	clientAnswer = message['answer']
+	ansData = {}
+	ansData['client'] = idClient
+	ansData['question'] = idQuestion
+	ansData['answer'] = clientAnswer
+	if roomReceivedAnswers.has_key(room) == True:
+		allAnswers = roomReceivedAnswers.get(room)
+		# Try to group ansData based on questionId and clientId
+		allAnswers.append(ansData)
+		roomReceivedAnswers[room] = allAnswers
+		sendReceivedAnswersToClient(room)
+	else:
+		allAnswers = []
+		allAnswers.append(ansData)
+		roomReceivedAnswers[room] = allAnswers
 
-
-
-# @socketio.on('client name', namespace='/test')
-# def test_message(message):
-# 	session['user'] = message['username']
-# 	emit('my response', {'data': message['username'] + message['data']})
-
-# @socketio.on('my event', namespace='/test')
-# def test_message(message):
-#     emit('my response', {'data': message['data']})
-
-# @socketio.on('my broadcast event', namespace='/test')
-# def test_message(message):
-#     emit('my response', {'data': message['data']}, broadcast=True)
-
-# @socketio.on('join', namespace='/test')
-# def on_join(data):
-#     room = data['room']
-#     join_room(room)
-#     if socketRooms.has_key(room) == True :
-#         usersInRoom = socketRooms.get(room)
-#         usersInRoom.append(session['id'])
-#         socketRooms[room] = usersInRoom
-#     else:
-#         usersInRoom = []
-#         usersInRoom.append(session['id'])
-#         socketRooms[room] = usersInRoom
-# 	clientName = session['user']
-# 	emit('my response', {'data' : clientName + ' has entered the room ' + room}, room=room)
-
-# @socketio.on('leave', namespace='/test')
-# def on_leave(data):
-#     # username = data['username']
-#     room = data['room']
-#     leave_room(room)
-#     if socketRooms.has_key(room) == True:
-#         usersInRoom = socketRooms.get(room)
-#         usersInRoom.remove(session['id'])
-#         socketRooms[room] = usersInRoom
-#     clientName = session['user']
-#     emit('my response', {'data' : clientName + ' has left the room ' + room}, room=room)
-
-# @socketio.on('my room event', namespace='/test')
-# def on_leave(message):
-#     room = message['room']
-#     clientName = session['user']
-#     flag = 0
-#     if socketRooms.has_key(room) == True:
-#         usersInRoom = socketRooms.get(room)
-#         for eachUser in usersInRoom:
-#             if eachUser == session['id'] :
-#                 flag = 1
-#                 emit('my response', {'data' : clientName + ' : ' + message['data']}, room=room)
-#                 break
-#     else:
-#         emit('my response', {'data' : "Not part of room"})
-#     if flag == 0:
-#         emit('my response', {'data' : "Not part of room"})
-
-# @socketio.on('send client', namespace='/test')
-# def test_message(message):
-#     sendToClient = message['client']
-#     for sessid, socket in request.namespace.socket.server.sockets.items():
-#         # emit('my response', {'data': 'sessions name ' + socket['/test'].session['id']})
-#         if socket['/test'].session['id'] == sendToClient:
-#             socket['/test'].base_emit('my response', {'data': message['data']})
+def sendReceivedAnswersToClient(room):
+	dataToSend = roomReceivedAnswers.get(room)
+	emit('my response', {'data': json.dumps(dataToSend)}, room=room)
+	del roomReceivedAnswers[room]
