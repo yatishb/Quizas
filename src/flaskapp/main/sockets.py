b67a694239f48f3c6b612531b80fd45f2478d089
Yatish <yatishby@gmail.com>
2014-10-29 23:09:13 +0800

    socket handler that maintains a copy of answers chosen by client for all ques

--- src/flaskapp/main/sockets.py
+++ src/flaskapp/main/sockets.py
@@ -4,9 +4,9 @@ from flask.ext.socketio import emit, join_room, leave_room
 from .. import socketio
 
 socketRooms = {}
-roomReceivedAnswers = {}
+roomClientAnswers = {}
+roomSendAnswers = {}
 defaultRoom = str(0)
-roomIndex = 1
 
 
 @socketio.on('connect', namespace='/test')
@@ -25,10 +25,13 @@ def printSocketsConnected():
 	for sessid, socket in request.namespace.socket.server.sockets.items():
 		emit('my response', {'data': 'sessions uuid: ' + socket['/test'].session['id']})
 
+
+# room = str(uuid.uuid1())
+# This has to be chosen by a separate function
+# and then passed onto this function
 @socketio.on('joinroom', namespace='/test')
 def joinRoom(room):
 	if session['room'] == defaultRoom:
-		# room = str(roomIndex)
 		room = room['room']
 		session['room'] = room
 		join_room(room)
@@ -40,10 +43,11 @@ def joinRoom(room):
 			usersInRoom = []
 			usersInRoom.append(session['id'])
 			socketRooms[room] = usersInRoom
-		# roomIndex +=1
 		emit('my response', {'data': 'Joined room'})
 	else:
 		emit('my response', {'data': 'Already part of a room'})
+
+
 
 @socketio.on('leaveroom', namespace='/test')
 def leaveRoom():
@@ -59,9 +63,13 @@ def leaveRoom():
 	else:
 		emit('my response', {'data': 'wasn\'t part of a room'})
 
+
+# End of quiz
+# Clear the room
+# Update answers received by the clients to db
 @socketio.on('clearroom', namespace='/test')
-def clearRoom(room):
-	room = room['room']
+def clearRoom():
+	room = session['room']
 	if socketRooms.has_key(room) == True:
 		usersInRoom = socketRooms.get(room)
 		del socketRooms[room]
@@ -71,10 +79,15 @@ def clearRoom(room):
 					socket['/test'].session['room'] = defaultRoom
 					socket['/test'].leave_room(room)
 					socket['/test'].base_emit('my response', {'data': "cleared room"})
-		# emit('my response', {'data': "checking for room"}, room=room) # This doesn't send message to any client. Verified
+					socket['/test'].base_emit('my response', {'data': socket['/test'].session['room']})
+		emit('my response', {'data': "checking for room"}, room=room) # This doesn't send message to any client. Verified
 	else:
 		emit('my response', {'data': 'no such room'})
+	# This is to counter some error experienced
+	session['room'] = defaultRoom
 
+
+# Each response by the client for each question is handled here
 @socketio.on('readanswer', namespace='/test')
 def readAnswerByClient(message):
 	room = session['room']
@@ -85,18 +98,47 @@ def readAnswerByClient(message):
 	ansData['client'] = idClient
 	ansData['question'] = idQuestion
 	ansData['answer'] = clientAnswer
-	if roomReceivedAnswers.has_key(room) == True:
-		allAnswers = roomReceivedAnswers.get(room)
-		# Try to group ansData based on questionId and clientId
-		allAnswers.append(ansData)
-		roomReceivedAnswers[room] = allAnswers
-		sendReceivedAnswersToClient(room)
+	if roomClientAnswers.has_key(room) == True:
+		# Received replied from this room
+		roomArray = roomClientAnswers.get(room)
+		if roomArray.has_key(idQuestion) == True:
+			# This question has been encountered 
+			# atleast one client has already answered
+			qidArray = roomArray.get(idQuestion)
+			if qidArray.has_key(idClient) == False:
+				qidArray[idClient] = clientAnswer
+				# Also remember this answer to be sent later
+				allAnswers = roomSendAnswers.get(room)
+				allAnswers.append(ansData)
+				roomSendAnswers[room] = allAnswers
+				sendReceivedAnswersToClient(room)
+		else:
+			# This question hasn't been encountered
+			qidArray = {}
+			qidArray[idClient] = clientAnswer
+			roomArray[idQuestion] = qidArray
+			# Also remember this answer to be sent later
+			allAnswers = []
+			allAnswers.append(ansData)
+			roomSendAnswers[room] = allAnswers
 	else:
+		# This room hasn't been encountered
+		# Quiz in this room has just started
+		qidArray = {}
+		qidArray[idClient] = clientAnswer
+		roomArray = {}
+		roomArray[idQuestion] = qidArray
+		roomClientAnswers[room] = roomArray
+		# Also remember this answer to be sent later
 		allAnswers = []
 		allAnswers.append(ansData)
-		roomReceivedAnswers[room] = allAnswers
+		roomSendAnswers[room] = allAnswers
 
+
+# This function sends responses to both clients to proceed to next question
+# Is called only when the server has received a response 
+# from both clients for the same question
 def sendReceivedAnswersToClient(room):
-	dataToSend = roomReceivedAnswers.get(room)
+	dataToSend = roomSendAnswers.get(room)
 	emit('my response', {'data': json.dumps(dataToSend)}, room=room)
-	del roomReceivedAnswers[room]
+	del roomSendAnswers[room]
