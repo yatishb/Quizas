f9be60639108dea444e54b664589907ccb1e78b2
Richard Goulter <richard.goulter@gmail.com>
2014-11-01 15:08:54 +0800

    Implements Quizlet search for sets. 401's if doesn't have qzlt auth.

--- src/flaskapp/main/quizletsets.py
+++ src/flaskapp/main/quizletsets.py
@@ -6,7 +6,7 @@ import json
 
 import secrets
 
-from flask import request, redirect
+from flask import request, redirect, abort
 from . import main
 
 CONSUMER_TOKEN  = secrets.quizlet_client_id
@@ -18,7 +18,7 @@ CALLBACK_URL    = secrets.quizlet_redirect_url
 # Quizlet API, see:
 # https://quizlet.com/api/2.0/docs/sets#view
 @main.route('/sets/<set_id>')
-def show_user_profile(set_id):
+def get_quizlet_set(set_id):
 	if set_id[:8] == "quizlet:":
 		qzlt_set_id = set_id[8:]
 
@@ -28,8 +28,8 @@ def show_user_profile(set_id):
 
 		# Get ACCESS TOKEN from Cookies
 		qzlt_access_token = request.cookies.get("quizlet_access_token")
-
-		payload = {}
+		if qzlt_access_token == None:
+			abort(401)
 
 		# FIXME: Not sure how to pass access token to `requests` properly
 		qzlt_set_url = "https://api.quizlet.com/2.0/sets/" + qzlt_set_id
@@ -62,5 +62,44 @@ def show_user_profile(set_id):
 # /user/#user-id/sets/#set-id
 
 # /sets/search/#query
+# See:
+# https://quizlet.com/api/2.0/docs/searching-sets
+@main.route('/sets/search/<query>')
+def quizlet_search(query):
+	tokenUrl = "https://api.quizlet.com/oauth/token"
+	clientID = CONSUMER_TOKEN
+	keySecret = CONSUMER_SECRET
+
+	# Get ACCESS TOKEN from Cookies
+	qzlt_access_token = request.cookies.get("quizlet_access_token")
+	if qzlt_access_token == None:
+		abort(401)
+
+	# Params for the search
+	payload = {'q': query}
+
+	# FIXME: Not sure how to pass access token to `requests` properly
+	qzlt_search_url = "https://api.quizlet.com/2.0/search/sets"
+	req = requests.get(qzlt_search_url,
+	                   params = payload,
+	                   headers = {"Authorization": "Bearer " + qzlt_access_token})
+
+	if req.status_code != 200 :
+		return "Bad Request: " + req.text
+	else :
+		# Return ... as per spec?
+		# Can't, since the Quizlet API point doesn't return terms.
+		# Clientside can request indiv. set later.
+		search_result = json.loads(req.text)
+
+		# Map from .sets to some return value
+		# Return [{id: "quizlet:SET_ID", name, size: TERM_COUNT}]
+		return json.dumps([{"id": "quizlet:" + str(qzlt_set["id"]),
+		                    "name": qzlt_set["title"],
+		                    # Not sure if every set has description
+		                    "description": qzlt_set.get("description"),
+		                    "size": qzlt_set["term_count"]} for qzlt_set in search_result["sets"]])
+
+
 
 
