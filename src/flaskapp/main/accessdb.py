7fc549cc72126e819fc27655625f6813aea2aba2
Yatish <yatishby@gmail.com>
2014-11-01 22:30:03 +0800

    rectified mistake in importing class models of db

--- src/flaskapp/main/accessdb.py
+++ src/flaskapp/main/accessdb.py
@@ -9,8 +9,8 @@ def documentGame(room, roomClientAnswers, flashsetId) :
 	userNames = allQuestions[0][1].keys()
 	user1 = userNames[0]
 	user2 = userNames[1]
-	gameUser1 = FlashGame(room, flashsetId, user1)
-	gameUser2 = FlashGame(room, flashsetId, user2)
+	gameUser1 = FG(room, flashsetId, user1)
+	gameUser2 = FG(room, flashsetId, user2)
 	db.session.add(gameUser1)
 	db.session.add(gameUser2)
 
@@ -22,8 +22,8 @@ def documentGame(room, roomClientAnswers, flashsetId) :
 		user1IsCorrect = True # Must check whether ans is correct
 		user2IsCorrect = True # Must check whether ans is correct
 
-		cardUser1 = FlashCardInGame(room, flashsetId, questionId, user1, user1Ans, user1IsCorrect)
-		cardUser2 = FlashCardInGame(room, flashsetId, questionId, user2, user2Ans, user2IsCorrect)
+		cardUser1 = FC(room, flashsetId, questionId, user1, user1Ans, user1IsCorrect)
+		cardUser2 = FC(room, flashsetId, questionId, user2, user2Ans, user2IsCorrect)
 		db.session.add(cardUser1)
 		db.session.add(cardUser2)
 
