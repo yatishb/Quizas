3901f4a2027a2f7729f6bc7cef70da9e3f7976c7
Yatish <yatishby@gmail.com>
2014-11-04 18:11:48 +0800

    making use of authhelper's lookup() for stats endpoints

--- src/flaskapp/main/statistics.py
+++ src/flaskapp/main/statistics.py
@@ -1,26 +1,47 @@
 from . import main
 from flask import request
-import accessdb
+import accessdb, authhelper
 
 @main.route('/user/<userid>/stats', methods=['GET'])
 def displayUserStats(userid):
-	return accessdb.getUserWinLossStats(userid)
+	internalUserid = authhelper.lookup(userid)
+	if internalUserid == None:
+		abort(401)
+	else:
+		return accessdb.getUserWinLossStats(internalUserid)
 
 @main.route('/user/<userid>/stats/vs/<opponent>', methods=['GET'])
 def displayUserStatsHeadToHead(userid, opponent):
-	return accessdb.getUserWinLossStats(userid, opponent)
+	internalUserid = authhelper.lookup(userid)
+	opponentUserid = authhelper.lookup(opponent)
+	if internalUserid == None or opponentUserid == None:
+		abort(401)
+	else:
+		return accessdb.getUserWinLossStats(internalUserid, opponentUserid)
 
 @main.route('/user/<userid>/stats/games', methods=['GET'])
 def displayUserGamesPlayed(userid):
-	return accessdb.getUserGames(userid)
+	internalUserid = authhelper.lookup(userid)
+	if internalUserid == None:
+		abort(401)
+	else:
+		return accessdb.getUserGames(internalUserid)
 
 @main.route('/user/<userid>/stats/game/<gameid>', methods=['GET'])
 def displayUserGameStats(userid, gameid):
-	return accessdb.getGameStats(userid, gameid)
+	internalUserid = authhelper.lookup(userid)
+	if internalUserid == None:
+		abort(401)
+	else:
+		return accessdb.getGameStats(userid, gameid)
 
 @main.route('/user/<userid>/stats/sets/<setid>', methods=['GET'])
 def displayUserSetStats(userid, setid):
-	return accessdb.getUserSetStats(userid, setid)
+	internalUserid = authhelper.lookup(userid)
+	if internalUserid == None:
+		abort(401)
+	else:
+		return accessdb.getUserSetStats(userid, setid)
 
 @main.route('/user/<userid>/gameresults', methods=['POST'])
 def readStatsOfJustEndedGame(userid):
