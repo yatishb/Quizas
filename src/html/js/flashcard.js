90286036db070210cf705440d91dc8d792ad6692
Yuanda <superyouyun@gmail.com>
2014-11-05 17:06:58 +0800

    Updated flashcard page

--- src/html/js/flashcard.js
+++ src/html/js/flashcard.js
@@ -18,23 +18,27 @@ $(document).ready(function() {
 });
 
 $(document).ajaxComplete(function() {
-    var element = $('.flashcards');
+    var top_bar = $('.top_bar');
+    top_bar.html(content.name);
+
+    var flashcards = $('.flashcards');
     for (var i = 0; i < content.cards.length; i++) {
-        element.append(
+        flashcards.append(
             "<div class='simple_card' id='" +
             i +
-            "'><div class='card_question'>" +
+            "'><div class='flipper'><div class='card_content question'><p class='text_field'>" +
                 content.cards[i].question +
-                "</div><div class='card_answer'>" +
+                "</div><div class='card_content answer'><p>" +
                 content.cards[i].answer +
-                "</div></div>"
+                "</div></div></div>"
         );
     };
 });
 
-function getStudySetContent(){
+function getStudySetContent() {
     $.get("/api/sets/quizlet:24957714", function(data) {
            content = JSON.parse(data);
+           console.log(content);
            if(content != null)
                console.log("Set data have been fetched.");
            else
@@ -44,3 +48,19 @@ function getStudySetContent(){
         alert("error in getStudySetContent call back function");
     });
 }
+
+$('.flashcards').on('tap', '.simple_card', function() {
+    // var cardID= this.id;
+    // var card = $('#'+cardID).find('.flipper');
+    //this.classList.toggle('hover');
+    var card = $(this).find('.flipper');
+    if(card.hasClass('flipped')) {
+        card.removeClass('flipped');
+    } else {
+        card.addClass('flipped');
+    }
+});
+
+$('.back_button').on("tap", function(){
+    window.location.href="study.html";
+});
