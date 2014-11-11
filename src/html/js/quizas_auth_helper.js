9732637e3ac9d2a9e17a5f1376dffd57c1fd400d
Richard Goulter <richard.goulter@gmail.com>
2014-11-11 20:44:20 +0800

    Adding scripts to support FB integration on client.

--- src/html/js/quizas_auth_helper.js
+++ src/html/js/quizas_auth_helper.js
@@ -1,7 +1,7 @@
 // Check which "sites" user already authorised with
 // e.g. ["twitter"]
 function quizas_authorized_sites() {
-    var quizas_auth_sites = ["quizlet", "twitter"];
+    var quizas_auth_sites = ["quizlet", "twitter", "facebook"];
     return quizas_auth_sites.filter(function (s) {
         return $.cookie(s + "_user_id") !== undefined;
     });
@@ -16,4 +16,34 @@ function quizas_user_id() {
     var site = authed_sites[0];
     var user_id = site + ":" + $.cookie(site + "_user_id");
     return user_id; // e.g. `quizlet:rgoulter`
+}
+
+
+function quizas_update_auth_cookies(site, authId, authToken) {
+    // site e.g. "quizlet", "twitter", "facebook"
+
+    // Check what other cookies Quizas has,
+    // if they clash (has this site, but different authId), remove the others.
+    if ($.cookie(site + "_user_id") != authId) {
+        var auth_sites = ["quizlet", "twitter", "facebook"]; // BAD MAGIC
+        for (var i in auth_sites) {
+            var s = auth_sites[i];
+
+            // No great harm in removing a cookie which isn't there.
+            $.removeCookie(s + "_user_id", { path: "/" });
+            $.removeCookie(s + "_access_token", { path: "/" });
+        }
+    }
+
+    // Set the cookie
+    $.cookie(site + "_user_id",
+             authId,
+             { expires: 365, path: '/' });
+    $.cookie(site + "_access_token",
+             authToken,
+             { expires: 365, path: '/' });
+
+    // Notify Quizas app about the new auth.
+	// FIXME: This is a bit daft, isn't it? Fix for G+ integ.
+    $.post("/api/facebookauthnotify");
 }
