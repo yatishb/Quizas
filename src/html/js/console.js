13fb98c4491d2c8e261728afc28719097e007b87
Yatish <yatishby@gmail.com>
2014-11-18 13:37:27 +0800

    fixed enemy score issue. working on console.html to display leaderboard

--- src/html/js/console.js
+++ src/html/js/console.js
@@ -1,5 +1,22 @@
 $(document).ready(function() {
-    var result = quizas_authorized_sites();
-    if(result.length == 0)
-        this.location.href='/index.html';
+	/*function outputProfile (profile) {
+        console.log("got here");
+        $('.profile_photo img').attr('src', profile.picture);
+        $('.name_content').text(profile.name);
+    }*/
+
+
+	$.get("/api/leaderboard", function (data) {
+		console.log("In jQuery GET callback:");
+		var obj = JSON.parse(data);
+		leaderboard = obj.data ;
+		for (i = 0; i < leaderboard.length; i++) {
+			id = leaderboard[i].id;
+			points = leaderboard[i].points;
+
+			//quizas_get_profile_for(id, outputProfile);
+			console.log("called profile pic func");
+		}
+    })
+
 });
